1129caad89b218546833255c76c1ad1391f72995
haroon <haroonsin@gmail.com>
2022-12-27 18:23:14 +0400

    mdc-102 changes

--- mdc_100_series/lib/home.dart
+++ mdc_100_series/lib/home.dart
@@ -13,22 +13,149 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'model/product.dart';
+import 'model/products_repository.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   // TODO: Make a collection of cards (102)
+  List<Card> _buildGridCards(BuildContext context) {
+    List<Product> products = ProductsRepository.loadProducts(Category.all);
+
+    if (products.isEmpty) {
+      return const <Card>[];
+    }
+
+    final ThemeData theme = Theme.of(context);
+    final NumberFormat formatter = NumberFormat.simpleCurrency(
+        locale: Localizations.localeOf(context).toString());
+
+    return products.map((product) {
+      return Card(
+        clipBehavior: Clip.antiAlias,
+        // TODO: Adjust card heights (103)
+        child: Column(
+          // TODO: Center items on the card (103)
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 18 / 11,
+              child: Image.asset(
+                product.assetName,
+                package: product.assetPackage,
+                // The images are drawn with a BoxFit of .scaleDown by default (in this case).
+                // Let's change that to .fitWidth so they zoom in a little and remove the extra whitespace.
+                fit: BoxFit.fitWidth,
+                // TODO: Adjust the box size (102)
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                child: Column(
+                  // TODO: Align labels to the bottom and center (103)
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  // TODO: Change innermost Column (103)
+                  children: <Widget>[
+                    // TODO: Handle overflowing labels (103)
+                    Text(
+                      product.name,
+                      style: theme.textTheme.headline6,
+                      maxLines: 1,
+                    ),
+                    const SizedBox(height: 8.0),
+                    Text(
+                      formatter.format(product.price),
+                      style: theme.textTheme.subtitle2,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }).toList();
+
+    // return List.generate(count, (int index) {
+    //   return Card(
+    //     clipBehavior: Clip.antiAlias,
+    //     child: Column(
+    //       crossAxisAlignment: CrossAxisAlignment.start,
+    //       children: <Widget>[
+    //         AspectRatio(
+    //           aspectRatio: 18.0 / 11.0,
+    //           child: Image.asset('assets/diamond.png'),
+    //         ),
+    //         Padding(
+    //           padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+    //           child: Column(
+    //             crossAxisAlignment: CrossAxisAlignment.start,
+    //             children: const [
+    //               Text('Title'),
+    //               SizedBox(height: 8.0),
+    //               Text('Description')
+    //             ],
+    //           ),
+    //         )
+    //       ],
+    //     ),
+    //   );
+    // });
+  }
+
   // TODO: Add a variable for Category (104)
   @override
   Widget build(BuildContext context) {
     // TODO: Return an AsymmetricView (104)
     // TODO: Pass Category variable to AsymmetricView (104)
-    return const Scaffold(
+    return Scaffold(
       // TODO: Add app bar (102)
-      // TODO: Add a grid view (102)
-      body: Center(
-        child: Text('You did it!'),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(
+            Icons.menu,
+            semanticLabel: "Menu Button",
+          ),
+          onPressed: () {},
+        ),
+        title: const Text("Shrine"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              print('Search button clicked');
+            },
+            icon: const Icon(
+              Icons.search,
+              semanticLabel: "Search",
+            ),
+          ),
+          IconButton(
+            onPressed: () {
+              print('Filter button clicked');
+            },
+            icon: const Icon(
+              Icons.tune,
+              semanticLabel: "Filter",
+            ),
+          ),
+        ],
       ),
+      // TODO: Add a grid view (102)
+      body: GridView.count(
+        crossAxisCount: 2,
+        padding: const EdgeInsets.all(16.0),
+        //The childAspectRatio: field identifies the size of the items based on an aspect ratio (width over height).
+        childAspectRatio: 8.0 / 9.0,
+        // TODO: Build a grid of cards (102)
+        children: _buildGridCards(context),
+      ),
+      // body: Center(
+      //   child: Text('You did it!'),
+      // ),
       // TODO: Set resizeToAvoidBottomInset (101)
       //Doing this ensures that the keyboard's appearance does not alter the size of the home page or its widgets.
       resizeToAvoidBottomInset: false,
